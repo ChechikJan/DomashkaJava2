d91d12a624a0eaf3b160cf82abd866cf26034eee
Danila Kalay <ChechikLud@gmail.com>
2022-06-15 14:11:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiketPriсe = 5000;
+        int bonusMiles = tiketPriсe / 20;
+        System.out.println("Бонусные мили = " +  bonusMiles);
+
+    }
+}
